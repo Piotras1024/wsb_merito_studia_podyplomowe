053885e40db37affa10abddf7fce75dc251dff97
Piotr <piotr.w.dabrowski2@gmail.com>
2023-12-16 09:53:28 +0100

    sredni wzrost dla poszczegolnych miast

--- Zjazd_04/dane.py
+++ Zjazd_04/dane.py
@@ -7,7 +7,27 @@ with open('dane.csv', 'r', encoding='utf8') as file:
 #print(lines)
 
 data = [line.strip().split(',') for line in lines ]
-
-
 average_high = sum(int(line[2]) for line in data) / len(data)
-print(average_high)
+
+total_height = 0
+i = 0
+
+
+heights_city = {}
+
+for row in data:
+    city = row[4]
+    height = int(row[2])
+
+    if city not in heights_city:
+        heights_city[city] = [height]
+    else:
+        heights_city[city].append(height)
+
+for city in heights_city.keys():
+    average_high = sum(heights_city[city])/len(heights_city[city])
+    print(city, "średni wzrosty  w tym mieście to :", average_high)
+
+
+
+
