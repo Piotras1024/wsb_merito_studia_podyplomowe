52280610cd4af696ab7fdc1db8ed6d94e5a5030c
Piotr <piotr.w.dabrowski2@gmail.com>
2023-12-16 10:39:54 +0100

    zadania zakonczone, wstep przed obiektowym pogladem

--- Zjazd_04/dane.py
+++ Zjazd_04/dane.py
@@ -14,20 +14,33 @@ i = 0
 
 
 heights_city = {}
+age_city = {}
+weight_city = {}
 
 for row in data:
     city = row[4]
     height = int(row[2])
+    age = int(row[1])
+    weight = int(row[3])
 
     if city not in heights_city:
         heights_city[city] = [height]
+        weight_city[city] = [weight]
+        age_city[city] = [age]
     else:
         heights_city[city].append(height)
+        age_city[city].append(age)
+        weight_city[city].append(weight)
 
 for city in heights_city.keys():
     average_high = sum(heights_city[city])/len(heights_city[city])
-    print(city, "średni wzrosty  w tym mieście to :", average_high)
+    average_weight = sum(weight_city[city])/len(weight_city[city])
+    average_age = sum(age_city[city])/len(age_city[city])
+    print(city, "średni wzrost  w tym mieście to :", round(average_high, 2))
+    print(city, "średni wiek  w tym mieście to :", round(average_age, 2))
+    print(city, "średni waga  w tym mieście to :", round(average_weight, 2))
 
 
+print(age_city["Warszawa"])
 
 
